50c9f12db0de9669a1057862ed71a411a61a6510
dropout0 <arayastr@gmail.com>
2023-12-13 18:47:31 -0300

    added if block to fetch tokens only for whitelisted commands only

--- poetry_private_source_plugin/plugin.py
+++ poetry_private_source_plugin/plugin.py
@@ -22,21 +22,29 @@ class PrivateSourcePlugin(ApplicationPlugin):
     def prepare_for_private_source(self, event: ConsoleCommandEvent, event_name: str, dispatcher: EventDispatcher) -> None:
         """Method at charge of checking current project's sources and fill the required poetry env vars"""
         command = event.command
-        if not isinstance(command, EnvCommand):
+
+        commands_whitelist = ["add","install","lock","remove","update"]
+
+        if command.name in commands_whitelist:
+
+            if not isinstance(command, EnvCommand):
+                return
+
+            for src in event.command.poetry.get_sources():
+                try:
+                    repo = SourceParserFactory(src.url)
+                except (ValueError, AttributeError):
+                    # Not a valid repository or maybe src.url doesn't exist
+                    # In both cases just continue
+                    continue
+
+                env_var_prefix = poetry_source_env_var_prefix(src.name)
+                env_username = f"{env_var_prefix}_USERNAME"
+                env_password = f"{env_var_prefix}_PASSWORD"
+
+                if not os.getenv(env_password):
+                    os.environ[env_username] = repo.username
+                    os.environ[env_password] = repo.password
+
+        else:
             return
-
-        for src in event.command.poetry.get_sources():
-            try:
-                repo = SourceParserFactory(src.url)
-            except (ValueError, AttributeError):
-                # Not a valid repository or maybe src.url doesn't exist
-                # In both cases just continue
-                continue
-
-            env_var_prefix = poetry_source_env_var_prefix(src.name)
-            env_username = f"{env_var_prefix}_USERNAME"
-            env_password = f"{env_var_prefix}_PASSWORD"
-
-            if not os.getenv(env_password):
-                os.environ[env_username] = repo.username
-                os.environ[env_password] = repo.password
